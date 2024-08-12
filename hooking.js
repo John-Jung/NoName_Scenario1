5845a02ad8943d75d5049ba7e045000bf7823300
sungwuk <sungwuk98@gmail.com>
2024-08-12 10:43:03 +0900

    second commit

--- hooking.js
+++ hooking.js
@@ -2,7 +2,7 @@ Java.perform(function() {
     let WebAppInterface = Java.use("com.example.nonameapp.WebViewActivity$WebAppInterface");
     WebAppInterface["getToken"].implementation = function () {
         console.log(`WebAppInterface.getToken is called`);
-        let json = "{\"autoLogin\": true, \"token\": \"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYmNAbmF2ZXIuY29tIiwiaWF0IjoxNzIxODg4MjY5LCJleHAiOjE3MjE5NzQ2Njl9.KSva7IyuK6sqH74yUhkHOnXiomhxS59FQXnRC04fvdE\"}";
+        let json = "{\"autoLogin\": true, \"token\": \"탈취한 jwt 토큰"}";
         return json;
     };
 });
